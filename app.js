76c6e0af3032ff0e29f778e74b1f08b72d20e57d
marcodem11 <marcodemicheleta01@outlook.it>
2022-02-17 09:28:01 +0100

    step 2

--- app.js
+++ app.js
@@ -3,4 +3,43 @@ const playButton = document.getElementById('play');
 
 const startGame = () => {
     console.log('Inizia il gioco');
+
+    const mode = parseInt (selectElement.value)
+    let rows;
+    let columns;
+    let cellSize;
+    console.log(mode)
+
+    switch(mode) {
+        case 1:
+            rows = 10;
+            columns = 10;
+            break;
+        case 2:
+            rows = 9;
+            columns = 9;
+            break;
+        case 3:
+            rows = 7;
+            columns = 7;
+            break;
+        default:
+            rows = 10;
+            columns = 10;
+    }
+
+    const cellNumber = rows * columns;
+    cellSize = `calc(100% / ${columns})`;
+    console.log (rows, columns, cellNumber);
+
+    gridElement.innerHTML = '';
+    
+    for( let i=0; i< cellNumber; i++) {
+        const cell = document.createElement('div');
+        cell.style.width = cellSize;
+        cell.append(i + 1);
+        cell.classList.add('cell');
+        gridElement.appendChild(cell);
+    }
+
 }
